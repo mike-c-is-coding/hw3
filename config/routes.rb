658d78e5af33733e8c4892e7db6845a457080fe8
Michael Carter <mike.carter@kellogg.northwestern.edu>
2025-02-24 23:32:48 +0000

    created site, need to do the buttons and other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  #root "place#index"
-  get("/", { :controller => "place", :action => "index" })
+    root "places#index"
+    resources :places
+    resources :entries, only: [:new, :create]
+  get("/", { :controller => "places", :action => "index" })
   get("/place", { :controller => "place", :action => "index" })
   get("/entry", { :controller => "entry", :action => "index" })
   get("/application_record", { :controller => "application_record", :action => "index" })
