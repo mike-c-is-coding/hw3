89bca840c32cbd3574cc91eb2c9cc9c7610431d4
Michael Carter <mike.carter@kellogg.northwestern.edu>
2025-02-25 16:39:03 +0000

    removed more redundant code

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,4 @@ Rails.application.routes.draw do
     resources :places do
       resources :entries, only: [:new, :create]
     end
-  get("/", { :controller => "places", :action => "index" })
-  get("/places", { :controller => "places", :action => "index" })
-  get("/entry", { :controller => "entry", :action => "index" })
-  get("/application_record", { :controller => "application_record", :action => "index" })
-
 end
